5450fade46cce23dafd8331838742cdd27d7cbd8
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-14 16:01:13 -0500

    created variablesfor a game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,22 +27,37 @@ $(document).ready(function() {
     "zimbabwe"
   ];
 
-  var computerChoice = country(Math.floor(Math.random() * countries.length));
   //creating variables
-  var wins = 0;
-  var losses = 0;
-  var guessleft = 10;
-  var guessedWord = [];
+  var wins = 0; // number of wins
+  var losses = 0; //number os losses
+  var guessleft = 10; // number of guesses remaining
+  var guessedletters = []; // store the guessed letters
+  var guessingWord = []; //store the "_" and to be used to replace the word
+  var pauseGame = false; // when true game starts again
+  var computerChoice;
+
+  resetGame();
 
   //function runs whenever the user presses the key
   document.onkeyup = function(event) {
-    //determines which key was pressed
-    var userGuess = event.key.toLowerCase();
-
-    //alert the user to type only the alphabet keys
-    if (event.which < 65 || event.which > 90) {
-      alert("Only a to z characters are allowed");
-      return;
+    if (isAlpha(event.key) && !pauseGame) {
+      checkForLetter(event.key.toUpperCase());
     }
   };
+  //Game Function
+  //create variables that hold references to the places in the HTML where we want to display things.
+  //updates the HTML from the function
+  function updateScreen() {
+    var userWord = document.getElementById("currentWord");
+    var userwins = document.getElementById("totalWins");
+    var userloss = document.getElementById("totalloss");
+    var remainingLetters = document.getElementById("guessesleft");
+    var guessedLetters = document.getElementById("guessedletters");
+
+    userwins.innerText = "Wins :" + wins;
+    userloss.innerText = "Losses :" + losses;
+    remainingLetters.innerText = "Guesses Left :" + guessleft;
+    guessedLetters.innerText = "Guessed Letters :" + guessedletters;
+    userWord.innerText = "Guessed Word :" + guessingWord.join("");
+  }
 });
